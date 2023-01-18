ec8fa289ee597294f54a65d39e84fb0e82a90cd0
wang-stanley <wang4928@purdue.edu>
2023-01-17 22:29:03 -0500

    Completed loader component

---
+++ components/Loader.tsx
@@ -0,0 +1,4 @@
+// Loading Spinner
+export default function Loader({ show }) {
+    return show ? <div className="border-8 border-t-blue-600 border-white rounded-full w-14 h-14 animate-spin"></div> : null;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+import Loader from '../components/Loader';
+
 export default function Home() {
   return (
     <div>
       <h1>
         hi
       </h1>
+      <Loader show/>
     </div>
   )
 }
